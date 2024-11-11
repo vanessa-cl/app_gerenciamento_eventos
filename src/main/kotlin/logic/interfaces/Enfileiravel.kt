aeebb9e68263cde4b2c3ee8a9140609b9641f5e3
ijoaom <joaomanoel2020.1@gmail.com>
2024-11-10 21:29:20 -0300

    Update Enfileiravel.kt

--- src/main/kotlin/logic/interfaces/Enfileiravel.kt
+++ src/main/kotlin/logic/interfaces/Enfileiravel.kt
@@ -1,4 +1,11 @@
 package logic.interfaces
 
-interface Enfileiravel {
+interface Enfileiravel{
+    fun enfileirar(dado: T?)
+    fun desenfileirar(): T?
+    fun atualizar(dado: T?)
+    fun frente(): T?
+    fun estaVazia(): Boolean
+    fun estaCheia(): Boolean
+    fun imprimir(): String
 }
