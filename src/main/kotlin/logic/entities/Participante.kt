a84e52b7905f7c45962bc2b8fb77f8a4692394b9
ijoaom <joaomanoel2020.1@gmail.com>
2024-11-10 21:28:27 -0300

    Update Participante.kt

--- src/main/kotlin/logic/entities/Participante.kt
+++ src/main/kotlin/logic/entities/Participante.kt
@@ -1,4 +1,38 @@
 package logic.entities
 
 class Participante {
+    private var Int id
+    private var String nome
+    private var String email
+    private var String cpf
+    private var String cargo
+    private var AgendaPalestras agenda
+
+
+fun constructor(nome: String, email: String, cpf: String, cargo: String) {
+    this.nome = nome
+    this.email = email
+    this.cpf = cpf
+    this.cargo = cargo
+
 }
+
+    fun getNome(): String {
+        return nome
+    }
+
+    fun getEmail(): String {
+        return email
+    }
+
+    fun getCpf(): String {
+        return cpf
+    }
+
+    fun getCargo(): String {
+        return cargo
+    }
+
+    fun getPalestrasInscritas(): AgendaPalestras {
+        return agenda
+    }
