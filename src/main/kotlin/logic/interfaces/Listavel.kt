2a77e192d32df5e3497656765ac479bbbfa30be7
ijoaom <joaomanoel2020.1@gmail.com>
2024-11-10 21:29:39 -0300

    Update Listavel.kt

--- src/main/kotlin/logic/interfaces/Listavel.kt
+++ src/main/kotlin/logic/interfaces/Listavel.kt
@@ -1,4 +1,14 @@
 package logic.interfaces
 
 interface Listavel {
+    fun anexar(dado: T)
+    fun Inserir(posicao: Int, dado: T)
+    fun selecionar(posicao: Int): T?
+    fun selecionarTodos(): Array<T?>
+    fun buscarPosicao(dado: T?): Int
+    fun atualizar(posicao: Int, dado: T?)
+    fun apagar(posicao: Int): T?
+    fun apagarTodos(): Array<T?>
+    fun estaVazia(): Boolean
+    fun estaCheia(): Boolean
 }
