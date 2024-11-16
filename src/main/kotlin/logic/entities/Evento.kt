22c31e14b41eac2fb241a1c8d0cf7168a45d4985
vanessa-cl <vanessalima951@gmail.com>
2024-11-16 20:56:17 -0300

    adicionando entidade de evento

--- src/main/kotlin/logic/entities/Evento.kt
+++ src/main/kotlin/logic/entities/Evento.kt
@@ -1,4 +1,50 @@
 package logic.entities
 
-class Evento {
+import logic.collections.AgendaPalestras
+import util.StatusEnum
+import java.time.LocalDate
+
+class Evento(
+    private var id: Int,
+    private var nome: String,
+    private var descricao: String,
+    private var valorInscricao: Double,
+    private var dataInicio: LocalDate,
+    private var dataFim: LocalDate,
+
+) {
+    private var status = StatusEnum.PENDENTE
+    private var agenda = AgendaPalestras()
+
+    fun getId(): Int {
+        return id
+    }
+
+    fun getNome(): String {
+        return nome
+    }
+
+    fun getDescricao(): String {
+        return descricao
+    }
+
+    fun getDataInicio(): LocalDate {
+        return dataInicio
+    }
+
+    fun getDataFim(): LocalDate {
+        return dataFim
+    }
+
+    fun getValorInscricao(): Double {
+        return valorInscricao
+    }
+
+    fun getStatus(): StatusEnum {
+        return status
+    }
+
+    fun getAgenda(): AgendaPalestras {
+        return agenda
+    }
 }
