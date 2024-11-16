932b3e14748ee2a79e5368cd63ba435b74f83138
vanessa-cl <vanessalima951@gmail.com>
2024-11-16 20:51:34 -0300

    adicionando entidade de palestra e enum de status das palestras

--- src/main/kotlin/logic/entities/Palestra.kt
+++ src/main/kotlin/logic/entities/Palestra.kt
@@ -1,4 +1,82 @@
 package logic.entities
 
-class Palestra {
+import logic.collections.FilaEspera
+import logic.collections.ListaParticipantes
+import util.StatusEnum
+import java.time.LocalDate
+import java.time.LocalTime
+
+class Palestra(
+    private var id: Int,
+    private var titulo: String,
+    private var palestrante: String,
+    private var limiteParticipantes: Int,
+    private var local: String,
+    private var data: LocalDate,
+    private var horarioInicio: LocalTime,
+    private var horarioFim: LocalTime
+) {
+    private var status = StatusEnum.PENDENTE
+    private var participantes = ListaParticipantes()
+    private var filaEspera = FilaEspera()
+
+    fun getId(): Int {
+        return id
+    }
+
+    fun getTitulo(): String {
+        return titulo
+    }
+
+    fun getPalestrante(): String {
+        return palestrante
+    }
+
+    fun getLimiteParticipantes(): Int {
+        return limiteParticipantes
+    }
+
+    fun getLocal(): String {
+        return local
+    }
+
+    fun getData(): LocalDate {
+        return data
+    }
+
+    fun getStatus(): StatusEnum {
+        return status
+    }
+
+    fun getDuracao(): Int {
+        return horarioFim.hour - horarioInicio.hour
+    }
+
+    fun getHorarioInicio(): LocalTime {
+        return horarioInicio
+    }
+
+    fun getHorarioFim(): LocalTime {
+        return horarioFim
+    }
+
+    fun getParticipantes(): ListaParticipantes {
+        return participantes
+    }
+
+    fun getFilaEspera(): FilaEspera {
+        return filaEspera
+    }
+
+    fun setHorarioInicio(horarioInicio: LocalTime) {
+        this.horarioInicio = horarioInicio
+    }
+
+    fun setHorarioFim(horarioFim: LocalTime) {
+        this.horarioFim = horarioFim
+    }
+
+    fun setStatus(status: StatusEnum) {
+        this.status = status
+    }
 }
--- src/main/kotlin/util/StatusEnum.kt
+++ src/main/kotlin/util/StatusEnum.kt
@@ -1,4 +1,5 @@
 package util
 
 enum class StatusEnum {
+    PENDENTE, CANCELADO, EM_ANDAMENTO, FINALIZADO
 }
