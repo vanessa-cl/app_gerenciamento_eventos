72fa33ed050b7174b89c4bb49a271c83738745dd
vanessa-cl <vanessalima951@gmail.com>
2024-11-16 21:44:26 -0300

    implementação de lista de participantes

--- src/main/kotlin/logic/collections/ListaParticipantes.kt
+++ src/main/kotlin/logic/collections/ListaParticipantes.kt
@@ -1,4 +1,54 @@
 package logic.collections
 
+import logic.entities.Participante
+import logic.structures.ListaEstatica
+
 class ListaParticipantes {
+    private var participantes = ListaEstatica<Participante>()
+
+    fun inserirParticipante(participante: Participante) {
+        participantes.anexar(participante)
+    }
+
+    fun removerParticipantePeloNome(nome: String): Participante? {
+        var participante = buscarParticipantePeloNome(nome)
+        if (participante != null) {
+            participantes.apagar(participantes.buscarPosicao(participante))
+            return participante
+        }
+        return null
+    }
+
+    fun buscarParticipantePeloNome(nome: String): Participante? {
+        for (i in 0 until participantes.selecionarTodos().size) {
+            val participante = participantes.selecionar(i)
+            if (participante != null && participante.getNome() == nome) {
+                return participante
+            }
+        }
+        return null
+    }
+
+    fun buscarParticipantePeloId(id: Int): Participante? {
+        for (i in 0 until participantes.selecionarTodos().size) {
+            val participante = participantes.selecionar(i)
+            if (participante != null && participante.getId() == id) {
+                return participante
+            }
+        }
+        return null
+    }
+
+    fun buscarTodosParticipantes(): Array<Participante?> {
+        return participantes.selecionarTodos()
+    }
+
+    fun estaCheia(): Boolean {
+        return participantes.estaCheia()
+    }
+
+    fun estaVazia(): Boolean {
+        return participantes.estaVazia()
+    }
+
 }
